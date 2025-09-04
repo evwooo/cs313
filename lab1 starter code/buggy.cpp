ed9dab13ca0fe34c75d51c9e60f3ace31484c683
evwooo <owenzhang.04@tamu.edu>
2025-09-04 18:33:26 +0000

    updated progress

--- lab1 starter code/buggy.cpp
+++ lab1 starter code/buggy.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 struct Point {
     int x, y;
@@ -19,7 +20,7 @@ class Shape {
     ~Shape () {
     }
 
-    void addPoints (/* formal parameter for unsized array called pts */) {
+    void addPoints (Point* pts) {
         for (int i = 0; i <= vertices; i++) {
             memcpy(points[i], &pts[i%vertices], sizeof(Point));
         }
@@ -30,12 +31,12 @@ class Shape {
         for (int i = 0; i <= vertices; i++) {
             // FIXME: there are two methods to access members of pointers
             //        use one to fix lhs and the other to fix rhs
-            int lhs = points[i].x * points[i+1].y;
-            int rhs = points[i+1].x * points[i].y;
+            int lhs = points[i]->x * points[i+1]->y; 
+            int rhs = (*points[i+1]).x * (*points[i]).y; // -> does the equivalent of (*<pointer>).
             temp += (lhs - rhs);
         }
         double area = abs(temp)/2.0;
-        return &area;
+        return &area; //dangling pointer?
     }
 };
 
@@ -45,6 +46,13 @@ int main () {
     //          tri1 = (0, 0)
     //          tri2 = (1, 2)
     //          tri3 = (2, 0)
+    Point tri1;
+    tri1.x = 0;
+    tri1.y = 0;
+
+    Point tri2 = {1,2};
+
+    Point tri3(2,0);
 
     // adding points to tri
     Point triPts[3] = {tri1, tri2, tri3};
@@ -53,10 +61,10 @@ int main () {
 
     // FIXME: create the following points using your preferred struct
     //        definition:
-    //          quad1 = (0, 0)
-    //          quad2 = (0, 2)
-    //          quad3 = (2, 2)
-    //          quad4 = (2, 0)
+    Point quad1(0, 0);
+    Point quad2(0, 2);
+    Point quad3(2, 2);
+    Point quad4(2, 0);
 
     // adding points to quad
     Point quadPts[4] = {quad1, quad2, quad3, quad4};
@@ -64,4 +72,6 @@ int main () {
     quad.addPoints(quadPts);
 
     // FIXME: print out area of tri and area of quad
+    std::cout << "Triangle area: " << tri->area() << std::endl;
+    std::cout << "Quadrilateral area: " << quad->area() << std::endl;
 }
